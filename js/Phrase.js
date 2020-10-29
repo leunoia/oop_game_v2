4f50b24a89522ed3e2c90001f03097f19abe6518
leunoia <68248433+leunoia@users.noreply.github.com>
2020-10-29 01:28:08 -0400

    first version of phrase script

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,34 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+const parentDiv = document.querySelector('#phrase ul')
+ class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+    addPhraseToDisplay (phrase){
+        const phraseArray = phrase.split();
+        phraseArray.forEach(phrase => {
+            const li = document.createElement('li');
+            if (phrase !== ' '){
+            li.className = `hide letter ${phrase}`
+            li.textContent = phrase; 
+            }else {
+            li.className = `space`  
+            li.textContent = phrase;
+            }
+            parentDiv.appendChild(li);
+        })
+    };
+    checkLetter(input) {
+       if (input === phrase){
+           return true;
+       }
+       return false;
+    }
+    showMatchedLetter() {
+        if(input === 'phrase'){
+            li.className = `show letter ${phrase}`
+        }
+    }
+ }
